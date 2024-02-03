4324e3b54c52bfeb2c638d9dc2af42537cd07fbe
coolpx <coolp_xels@proton.me>
2024-02-02 21:45:36 -0600

    Make server start message more informative

--- index.js
+++ index.js
@@ -98,5 +98,5 @@ app.get('/audio/:artist/:album/:title', async (request, response) => {
 
 // start server
 app.listen(port, () => {
-    console.log('Server started');
+    console.log(`Server started at ${url}`);
 });
